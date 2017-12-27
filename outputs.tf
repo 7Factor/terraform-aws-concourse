e464d26605b3d3a81e705233d275a877391316e4
jduv <jduv@7factor.io>
2017-12-26 23:35:07 -0500

    describing the outputs better

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "lb_dns" {
-  value = "${aws_elb.concourse_lb.dns_name}"
+  value       = "${aws_elb.concourse_lb.dns_name}"
+  description = "The DNS value of your ELB hosting the concourse cluster. Point your FQDN to it."
 }

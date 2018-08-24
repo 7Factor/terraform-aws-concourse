c112f215bfb96e886357698fcffa69fc399993b3
jduv <jduv@7factor.io>
2018-08-23 21:03:09 -0400

    Swapping sg

--- sg.tf
+++ sg.tf
@@ -53,8 +53,8 @@ resource "aws_security_group_rule" "allow_worker_to_web" {
   from_port = 2222
   to_port = 2222
   protocol = "tcp"
-  source_security_group_id = "${aws_security_group.web_sg.id}"
-  security_group_id = "${aws_security_group.worker_sg.id}"
+  source_security_group_id = "${aws_security_group.worker_sg.id}"
+  security_group_id = "${aws_security_group.web_sg.id}"
 }
 
 resource "aws_security_group" "worker_sg" {

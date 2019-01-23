0ae7ce74bd150b7ffd861361a99cd0516bfac72a
jduv <jduv@7factor.io>
2019-01-22 23:44:51 -0500

    mend

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,12 @@ output "lb_dns" {
   description = "The DNS value of your LB hosting the concourse cluster. Point your FQDN to it."
 }
 
-output "web_sg" {
+output "web_sg_id" {
   value       = "${aws_security_group.web_sg.id}"
   description = "Security group for web boxes. Consume this by other modules as necessary--specifically locking down DB access."
+}
+
+output "web_asg_id" {
+  value = "${aws_autoscaling_group.web_asg.id}"
+  description = "ID of the web ASG so you can attach your own scaling policies."
 }

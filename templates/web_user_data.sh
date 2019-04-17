7b1937a4afd8d4451077b27b6f9161139f22d3ff
jduv <jduv@7factor.io>
2019-04-17 17:41:17 -0400

    Moving order of operations:

--- templates/web_user_data.sh
+++ templates/web_user_data.sh
@@ -13,29 +13,15 @@ sudo wget -P /etc/ https://github.com/concourse/concourse/releases/download/v${c
 sudo tar -xzf /etc/concourse-${conc_version}-linux-amd64.tgz --directory=/etc/
 
 sudo mkdir -p /etc/concourse/keys/web
-sudo chown -R ubuntu:ubuntu /etc/concourse
 
 # Dump keys into the correct place. Because terraform automatically
 # adds a newline to any files read in we need to use echo -n here.
-echo -n "${authorized_worker_keys}" > /etc/concourse/keys/web/authorized_worker_keys
-echo -n "${session_signing_key}" > /etc/concourse/keys/web/session_signing_key
-echo -n "${tsa_host_key}" > /etc/concourse/keys/web/tsa_host_key
+sudo echo -n "${authorized_worker_keys}" > /etc/concourse/keys/web/authorized_worker_keys
+sudo echo -n "${session_signing_key}" > /etc/concourse/keys/web/session_signing_key
+sudo echo -n "${tsa_host_key}" > /etc/concourse/keys/web/tsa_host_key
 sudo find /etc/concourse/keys/web -type f -exec chmod 400 {} \;
 
-echo /etc/concourse/bin/concourse web \
---peer-address http://$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4):8080 \
---postgres-host ${concdb_host} \
---postgres-port ${concdb_port} \
---postgres-user ${concdb_user} \
---postgres-password ${concdb_password} \
---postgres-database ${concdb_database} \
---external-url https://${conc_fqdn} \
---tsa-session-signing-key /etc/concourse/keys/web/session_signing_key \
---tsa-host-key /etc/concourse/keys/web/tsa_host_key \
---tsa-authorized-keys /etc/concourse/keys/web/authorized_worker_keys \
-${authentication_config} \
-${cred_store_config} \
-${web_launch_options}
+sudo chown -R ubuntu:ubuntu /etc/concourse
 
 /etc/concourse/bin/concourse web \
 --peer-address http://$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4):8080 \
--- templates/worker_user_data.sh
+++ templates/worker_user_data.sh
@@ -14,24 +14,15 @@ sudo tar -xzf /etc/concourse-${conc_version}-linux-amd64.tgz --directory=/etc/
 
 sudo mkdir -p /etc/concourse/keys/worker
 sudo mkdir -p /concourse-tmp
-sudo chown -R ubuntu:ubuntu /etc/concourse
-sudo chown -R ubuntu:ubuntu /concourse-tmp
 
 # Dump keys into the correct place. Because terraform automatically
 # adds a newline to any files read in we need to use echo -n here.
-echo -n "${tsa_public_key}" > /etc/concourse/keys/worker/tsa_host_key.pub
-echo -n "${worker_key}" > /etc/concourse/keys/worker/worker_key
+sudo echo -n "${tsa_public_key}" > /etc/concourse/keys/worker/tsa_host_key.pub
+sudo echo -n "${worker_key}" > /etc/concourse/keys/worker/worker_key
 sudo find /etc/concourse/keys/worker -type f -exec chmod 400 {} \;
 
-echo /etc/concourse/bin/concourse worker \
---bind-ip 0.0.0.0 \
---baggageclaim-bind-ip 0.0.0.0 \
---tsa-host ${tsa_host}:2222 \
---tsa-public-key /etc/concourse/keys/worker/tsa_host_key.pub \
---tsa-worker-private-key /etc/concourse/keys/worker/worker_key \
---work-dir /concourse-tmp \
---garden-dns-proxy-enable \
-${worker_launch_options}
+sudo chown -R ubuntu:ubuntu /etc/concourse
+sudo chown -R ubuntu:ubuntu /concourse-tmp
 
 /etc/concourse/bin/concourse worker \
 --bind-ip 0.0.0.0 \

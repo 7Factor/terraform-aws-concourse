7590d8358774363c508d87ad11cf027760e894d4
jduv <jduv@7factor.io>
2019-04-18 11:11:40 -0400

    Updating outputs for easier consumption from other modules

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,13 @@ output "web_asg" {
   value       = "${aws_autoscaling_group.web_asg.id}"
   description = "ID of the web ASG so you can attach your own scaling policies."
 }
+
+output "utility_accessible_sg" {
+  value       = "${var.utility_accessible_sg}"
+  description = "Utility accessible SG for consumption by other terraform."
+}
+
+output "vpc_id" {
+  value       = "${var.vpc_id}"
+  description = "The VPC id that concourse was installed in for consumption by other terraform."
+}

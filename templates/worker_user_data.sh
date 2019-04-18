e7359eb94fbf115aa97a61e6898bcc9385d0c443
jduv <jduv@7factor.io>
2019-04-18 09:14:29 -0400

    Forwarding IPTables

--- templates/worker_user_data.sh
+++ templates/worker_user_data.sh
@@ -20,6 +20,7 @@ sudo echo -n "${tsa_public_key}" > /etc/concourse/keys/worker/tsa_host_key.pub
 sudo echo -n "${worker_key}" > /etc/concourse/keys/worker/worker_key
 sudo find /etc/concourse/keys/worker -type f -exec chmod 400 {} \;
 
+sudo iptables -P FORWARD ACCEPT
 sudo chown -R ubuntu:ubuntu /etc/concourse
 sudo chown -R ubuntu:ubuntu /concourse-tmp
 

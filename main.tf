b6be253d08f1e191ad6f7ae2878f4dd77a982f69
jduv <jduv@7factor.io>
2019-04-29 20:46:32 -0400

    Updating AMI to the correct format

--- main.tf
+++ main.tf
@@ -6,14 +6,20 @@ terraform {
 # Swapping to a tied down ubuntu version for stability.
 data "aws_ami" "base_ami" {
   most_recent = true
-
-  filter {
-    name   = "owner-alias"
-    values = ["amazon"]
-  }
+  owners      = ["099720109477"]
 
   filter {
     name   = "name"
     values = ["amzn2-ami-hvm*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }

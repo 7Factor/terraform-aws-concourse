8fa4813438297e4767ae56b168cf4bfb1d6ca111
chris-malloy <chrismalloymusic@gmail.com>
2020-11-03 16:05:48 -0500

    Locking down concourse base ami to ubuntu 20.04 focal LTS to resolve kernel bug.

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ data "aws_ami" "base_ami" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-*-18.*-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
   filter {

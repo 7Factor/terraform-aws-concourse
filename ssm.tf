a7271b90e450ae72ba679d49693d890b51a8d2ae
Jeremy Duvall <jduv@7factor.io>
2021-10-27 23:07:30 -0400

    Adding a lot of missing stuff.

--- ssm.tf
+++ ssm.tf
@@ -1,3 +1,31 @@
+# Use AWS default patch baselines for Ubuntu
+data "aws_ssm_patch_baseline" "ubuntu_patch_baseline" {
+  owner            = "AWS"
+  name_prefix      = "AWS-"
+  operating_system = "UBUNTU"
+}
+
+# I'm doing this in locals because according to terraform's documentation
+# the return ID of an aws_ssm_patch_group is the name comma id (name,id).
+# That seems gross to parse out with a split call, so let's just tie them
+# together here with well organized code instead.
+locals {
+  web_patch_group_name    = "concourse-web"
+  worker_patch_group_name = "concourse-worker"
+}
+
+# Keeping patch groups together with this locals block to reduce confusion.
+resource "aws_ssm_patch_group" "web_patch_group" {
+  baseline_id = data.aws_ssm_patch_baseline.ubuntu_patch_baseline.id
+  patch_group = local.web_patch_group_name
+}
+
+resource "aws_ssm_patch_group" "worker_patch_group" {
+  baseline_id = data.aws_ssm_patch_baseline.ubuntu_patch_baseline.id
+  patch_group = local.worker_patch_group_name
+}
+
+# Web window, targets, and task
 resource "aws_ssm_maintenance_window" "web_window" {
   name              = "concourse-web-patch-window"
   schedule          = var.web_patch_schedule
@@ -6,12 +34,16 @@ resource "aws_ssm_maintenance_window" "web_window" {
   cutoff            = 0
 }
 
-resource "aws_ssm_maintenance_window" "worker_window" {
-  name              = "concourse-worker-patch-window"
-  schedule          = var.worker_patch_schedule
-  schedule_timezone = var.schedule_timezone
-  duration          = 1
-  cutoff            = 0
+resource "aws_ssm_maintenance_window_target" "web_targets" {
+  window_id     = aws_ssm_maintenance_window.web_window.id
+  name          = "concourse-web-patch-targets"
+  description   = "Patches concourse web boxes."
+  resource_type = "INSTANCE"
+
+  targets {
+    key    = "tag:Patch Group"
+    values = [local.web_patch_group_name]
+  }
 }
 
 resource "aws_ssm_maintenance_window_task" "patch_web_boxes" {
@@ -20,6 +52,32 @@ resource "aws_ssm_maintenance_window_task" "patch_web_boxes" {
   task_arn        = "AWS-RunPatchBaseline"
   task_type       = "AUTOMATION"
   window_id       = aws_ssm_maintenance_window.web_window.id
+
+  targets {
+    key    = "WindowTargetIds"
+    values = [aws_ssm_maintenance_window_target.web_targets.id]
+  }
+}
+
+# Worker window, targets, and task
+resource "aws_ssm_maintenance_window" "worker_window" {
+  name              = "concourse-worker-patch-window"
+  schedule          = var.worker_patch_schedule
+  schedule_timezone = var.schedule_timezone
+  duration          = 1
+  cutoff            = 0
+}
+
+resource "aws_ssm_maintenance_window_target" "worker_targets" {
+  window_id     = aws_ssm_maintenance_window.worker_window.id
+  name          = "concourse-worker-patch-targets"
+  description   = "Patches concourse worker boxes."
+  resource_type = "INSTANCE"
+
+  targets {
+    key    = "tag:Patch Group"
+    values = [local.worker_patch_group_name]
+  }
 }
 
 resource "aws_ssm_maintenance_window_task" "patch_worker_boxes" {
@@ -28,21 +86,9 @@ resource "aws_ssm_maintenance_window_task" "patch_worker_boxes" {
   task_arn        = "AWS-RunPatchBaseline"
   task_type       = "AUTOMATION"
   window_id       = aws_ssm_maintenance_window.worker_window.id
-}
 
-# Use AWS default patch baselines for Ubuntu
-data "aws_ssm_patch_baseline" "ubuntu_patch_baseline" {
-  owner            = "AWS"
-  name_prefix      = "AWS-"
-  operating_system = "UBUNTU"
-}
-
-resource "aws_ssm_patch_group" "web_patch_group" {
-  baseline_id = data.aws_ssm_patch_baseline.ubuntu_patch_baseline.id
-  patch_group = "concourse-web"
-}
-
-resource "aws_ssm_patch_group" "worker_patch_group" {
-  baseline_id = data.aws_ssm_patch_baseline.ubuntu_patch_baseline.id
-  patch_group = "concourse-worker"
+  targets {
+    key    = "WindowTargetIds"
+    values = [aws_ssm_maintenance_window_target.worker_targets.id]
+  }
 }

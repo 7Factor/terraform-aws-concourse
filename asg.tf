3bf24a700a5e5a4125c50efa896fcd17e4a42536
Jeremy Wood <jeremy.wood@7factor.io>
2024-02-23 15:12:48 -0500

    Revert "Trigger ASG replacement when user_data objects change."
    This reverts commit 0f41372dcd4894f85d81f1127645e2ed2c0ac133.

--- asg.tf
+++ asg.tf
@@ -101,7 +101,6 @@ resource "aws_autoscaling_group" "web_asg" {
   lifecycle {
     create_before_destroy = true
     ignore_changes        = [desired_capacity]
-    replace_triggered_by  = [aws_s3_object.web_user_data]
   }
 
   tag {
@@ -172,7 +171,6 @@ resource "aws_autoscaling_group" "worker_asg" {
   lifecycle {
     create_before_destroy = true
     ignore_changes        = [desired_capacity]
-    replace_triggered_by  = [aws_s3_object.worker_user_data]
   }
 
   tag {
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">=1.2"
+  required_version = ">=0.12.3"
 }
 
 data "aws_ami" "base_ami" {
